0cdcaa82f39b6a018a2c4f094312f2000d7e1919
YureNathan <yure.nathan303@gmail.com>
2024-11-01 22:22:45 -0300

    edited css and menu off canvas

---
+++ script.js
@@ -0,0 +1,33 @@
+const acessAPI =
+  "https://mock-api.driven.com.br/api/v6/uol/participants/5ad41206-f9b5-4be4-b498-e6b82295439c";
+const mensagem = {
+  hours: "20:31:01",
+  name: "jumo",
+  status: "entra na sala",
+  mensage: "olá",
+};
+function getUser() {
+  let usuario = prompt("Qual seu nome?");
+  const resposta = axios.post(`${acessAPI}`, { name: `${usuario}` });
+  resposta.then(sucess);
+  resposta.catch(error);
+  while (promess.status === 400) {
+    let usuario = prompt("Qual seu nome?");
+  }
+}
+
+function sucess(promess) {
+  console.log(promess);
+}
+
+function error(promess) {}
+// para entrar na sala
+// getUser();
+
+function getMensages() {
+  const ul = document.querySelector(".chatMensagem");
+  ul.innerHTML += `
+  <li>${mensagem.hours}</li>
+ `;
+}
+// getMensages();
